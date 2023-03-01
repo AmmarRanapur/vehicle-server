ca5fef5583f091bf69865f932210ea7df4fd638a
ranapura <ranapura@PUN-RANAPURA-L>
2023-03-01 11:39:17 +0530

    Add project files.

---
+++ SampleProj/Controllers/VehicleController.cs
@@ -0,0 +1,152 @@
+using Microsoft.AspNetCore.Mvc;
+using SampleProj.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+using Microsoft.AspNetCore.Cors;
+
+namespace SampleProj.Controllers
+{   
+
+    [ApiController]
+    [Route("[controller]")]
+    public class VehicleController : ControllerBase
+    {   
+
+        private string connectionString = @"Server=(localdb)\VehicleDB;Database=Vehicle;Trusted_Connection=True;";
+        
+        [HttpGet]
+        public string Get()
+        {
+            //Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            List<Vehicle> vehicles = new List<Vehicle>();
+           
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM VehicleDetails", connection);
+                SqlDataReader reader = command.ExecuteReader();
+              
+                while (reader.Read())
+                {
+                    Guid id = reader.GetGuid(0);
+                    string registration = reader.GetString(1);
+                    string year = reader.GetString(2);
+                    string make = reader.GetString(3);
+                    string model = reader.GetString(4);
+                    string info = reader.GetString(5);
+                    bool isActive = reader.GetBoolean(6);
+
+                    vehicles.Add(new Vehicle(id, registration, year, make, model, info, isActive));
+                }
+            }
+
+            return JsonConvert.SerializeObject(vehicles);
+        }
+
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            List<Vehicle> vehicles1 = new List<Vehicle>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM VehicleDetails where Id = '" + id + "'", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Guid Id = reader.GetGuid(0);
+                    string registration = reader.GetString(1);
+                    string year = reader.GetString(2);
+                    string make = reader.GetString(3);
+                    string model = reader.GetString(4);
+                    string info = reader.GetString(5);
+                    bool isActive = reader.GetBoolean(6);
+
+                    vehicles1.Add(new Vehicle(Id, registration, year, make, model, info, isActive));
+                }
+            }
+
+            return JsonConvert.SerializeObject(vehicles1);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE VehicleDetails SET IsActive=false WHERE Id=@id", connection);
+                command.Parameters.AddWithValue("@id", id);
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    return Ok($"Vehicle with ID {id} deleted successfully.");
+                }
+                else
+                {
+                    return NotFound($"Vehicle with ID {id} not found.");
+                }
+            }
+        }
+        [HttpPost]
+        public IActionResult Post(Vehicle vehicle)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO VehicleDetails VALUES (@id, @vin, @year, @make, @model, @description, @isActive)", connection);
+                command.Parameters.AddWithValue("@id", Guid.NewGuid());
+                command.Parameters.AddWithValue("@vin", vehicle.VIN);
+                command.Parameters.AddWithValue("@year", vehicle.Year);
+                command.Parameters.AddWithValue("@make", vehicle.Make);
+                command.Parameters.AddWithValue("@model", vehicle.Model);
+                command.Parameters.AddWithValue("@description", vehicle.Description);
+                command.Parameters.AddWithValue("@isActive", vehicle.IsActive);
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    return Ok($"Vehicle with registration {vehicle.VIN} added successfully.");
+                }
+                else
+                {
+                    return BadRequest("Unable to add vehicle.");
+                }
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id,Vehicle vehicle)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("UPDATE VehicleDetails SET VIN=@vin,year=@year,make=@make,model=@model,description=@description,isActive=@isActive WHERE id=@id", connection);
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@vin", vehicle.VIN);
+                command.Parameters.AddWithValue("@year", vehicle.Year);
+                command.Parameters.AddWithValue("@make", vehicle.Make);
+                command.Parameters.AddWithValue("@model", vehicle.Model);
+                command.Parameters.AddWithValue("@description", vehicle.Description);
+                command.Parameters.AddWithValue("@isActive", vehicle.IsActive);
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    return Ok($"Vehicle with Id {id} updated successfully.");
+                }
+                else
+                {
+                    return BadRequest("Unable to update vehicle.");
+                }
+            }
+        }
+    }
+}
---
+++ SampleProj/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace SampleProj.Models
+{
+    public class Vehicle
+    {   
+        public Vehicle(Guid id,string vin,string year,string make,string model,string description,bool isActive)
+        {
+            Id= id;
+            VIN=vin;   
+            Year= year;
+            Make= make;
+            Model= model;
+            Description= description;
+            IsActive=isActive;
+        }
+        public Guid Id { get; set; }
+        public string VIN { get; set; }
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
